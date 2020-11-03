728d2cc0d16cef3bbcd3d306323c3b479ee46271
Ersnix <tuadi0123@gmail.com>
2020-11-03 17:26:18 +0800

    edit template

--- web-perdagangan-laravel/resources/views/belanja.blade.php
+++ web-perdagangan-laravel/resources/views/belanja.blade.php
@@ -50,8 +50,8 @@
                         <div class="main-menu d-none d-lg-block">
                             <nav>                                                
                                 <ul id="navigation">  
-                                    <li><a href="index.html">Home</a></li>
-                                    <li><a href="shop.html">shop</a></li>
+                                    <li><a href="{{url('/')}}">Home</a></li>
+                                    <li><a href="{{url('/shop')}}">shop</a></li>
                                     <li><a href="about.html">about</a></li>
                                     <li class="hot"><a href="#">Latest</a>
                                         <ul class="submenu">
@@ -133,9 +133,6 @@
                                         <img src="{{ asset('/image/'.$j->gambar) }}" height="360px" alt="">
                                         <div class="img-cap">
                                             <span>Add to cart</span>
-                                        </div>
-                                        <div class="favorit-items">
-                                            <span class="flaticon-heart"></span>
                                         </div>
                                     </div>
                                     <div class="popular-caption">
@@ -366,7 +363,7 @@
             </div>
         </section>
         <!-- Latest Products End -->
-        <!--? Shop Method Start-->
+       <!--? Shop Method Start-->
         <div class="shop-method-area">
             <div class="container">
                 <div class="method-wrapper">
@@ -374,22 +371,22 @@
                         <div class="col-xl-4 col-lg-4 col-md-6">
                             <div class="single-method mb-40">
                                 <i class="ti-package"></i>
-                                <h6>Free Shipping Method</h6>
-                                <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                                <h6>GRATIS ongkir</h6>
+                                <p>Pengiriman Produk kami tidak memerlukan biaya tambahan</p>
                             </div>
                         </div>
                         <div class="col-xl-4 col-lg-4 col-md-6">
                             <div class="single-method mb-40">
                                 <i class="ti-unlock"></i>
-                                <h6>Secure Payment System</h6>
-                                <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                                <h6>Sistem Pembayaran yang aman</h6>
+                                <p>Kami menjamin Pembayaran yang anda lakukan di website kami</p>
                             </div>
                         </div> 
                         <div class="col-xl-4 col-lg-4 col-md-6">
                             <div class="single-method mb-40">
                                 <i class="ti-reload"></i>
-                                <h6>Secure Payment System</h6>
-                                <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                                <h6>Melayani Penukaran Barang</h6>
+                                <p>jika Anda merasa barang yang anda dapatkan tidak sama dengan yang dipesan kami siap melayani penukaran barang</p>
                             </div>
                         </div>
                     </div>
@@ -408,11 +405,11 @@
                             <div class="single-footer-caption mb-30">
                                 <!-- logo -->
                                 <div class="footer-logo">
-                                    <a href="index.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
+                                    <a href="index.html"><p>KLMPK Perdagangan</p></a>
                                 </div>
                                 <div class="footer-tittle">
                                     <div class="footer-pera">
-                                        <p>Asorem ipsum adipolor sdit amet, consectetur adipisicing elitcf sed do eiusmod tem.</p>
+                                        <p>Hmmm..... <br> Pokoknya kami adalah kelompok web perdagangan</p>
                                 </div>
                                 </div>
                             </div>
@@ -434,12 +431,12 @@
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-7">
                         <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
-                                <h4>New Products</h4>
+                                <h4>Jam Terbaru</h4>
                                 <ul>
-                                    <li><a href="#">Woman Cloth</a></li>
-                                    <li><a href="#">Fashion Accessories</a></li>
-                                    <li><a href="#"> Man Accessories</a></li>
-                                    <li><a href="#"> Rubber made Toys</a></li>
+                                <?php for ($k=0; $k < 3; $k++) { 
+                                ?>
+                                    <li><a href="#">{{$jam[$k]->nama}}</a></li>
+                                <?php } ?>
                                 </ul>
                             </div>
                         </div>
@@ -447,12 +444,13 @@
                     <div class="col-xl-3 col-lg-3 col-md-5 col-sm-7">
                         <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
-                                <h4>Support</h4>
+                                <h4>Tim</h4>
                                 <ul>
-                                    <li><a href="#">Frequently Asked Questions</a></li>
-                                    <li><a href="#">Terms & Conditions</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                    <li><a href="#">Report a Payment Issue</a></li>
+                                    <li><a href="#">I Putu Adi Perdana Utama</a></li>
+                                    <li><a href="#">I Made Dita Yoga Radia</a></li>
+                                    <li><a href="#">I Made Adi Wira Surya Kencana</a></li>
+                                    <li><a href="#">I Putu Ditya Purnama Putra</a></li>
+                                    <li><a href="#">I Gede Winkayana</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -463,17 +461,17 @@
                     <div class="col-xl-7 col-lg-8 col-md-7">
                         <div class="footer-copy-right">
                             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>                 
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> and edited by kelompok perdagangan
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>                  
                         </div>
                     </div>
                     <div class="col-xl-5 col-lg-4 col-md-5">
                         <div class="footer-copy-right f-right">
                             <!-- social -->
                             <div class="footer-social">
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-behance"></i></a>
+                                <a href="https://instagram.com/erx.tuadi" target="_blank"><i class="fab fa-instagram"></i></a>
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://wa.me/+6289697448955" target="_blank"><i class="fab fa-whatsapp"></i></a>
                                 <a href="#"><i class="fas fa-globe"></i></a>
                             </div>
                         </div>
@@ -492,10 +490,11 @@
             </form>
         </div>
     </div>
+    <div></div>
     <!-- Search model end -->
 
-<!-- JS here -->
-    <!-- All JS Custom Plugins Link Here here -->
+    <!-- JS here -->
+
     <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
     <!-- Jquery, Popper, Bootstrap -->
     <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
@@ -513,7 +512,7 @@
     <script src="./assets/js/animated.headline.js"></script>
     <script src="./assets/js/jquery.magnific-popup.js"></script>
 
-    <!-- Scroll up, nice-select, sticky -->
+    <!-- Scrollup, nice-select, sticky -->
     <script src="./assets/js/jquery.scrollUp.min.js"></script>
     <script src="./assets/js/jquery.nice-select.min.js"></script>
     <script src="./assets/js/jquery.sticky.js"></script>
@@ -525,7 +524,7 @@
     <script src="./assets/js/mail-script.js"></script>
     <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
     
-    <!-- Jquery Plugins, main Jquery -->	
+    <!-- Jquery Plugins, main Jquery -->    
     <script src="./assets/js/plugins.js"></script>
     <script src="./assets/js/main.js"></script>
     
